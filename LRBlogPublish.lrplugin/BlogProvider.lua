a599f0f3fe98f2516bb98f56f684478a45b5366c
Dave Townsend <dtownsend@oxymoronical.com>
2012-12-04 22:46:11 -0800

    Add account details to publish service setup dialog.

--- LRBlogPublish.lrplugin/BlogProvider.lua
+++ LRBlogPublish.lrplugin/BlogProvider.lua
@@ -4,7 +4,10 @@ License, v. 2.0. If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.
 ------------------------------------------------------------------------------]]
 
-local LrDialogs = import 'LrDialogs'
+local LrView = import 'LrView'
+
+local bind = LrView.bind
+local share = LrView.share
 
 local provider = { }
 
@@ -14,16 +17,133 @@ provider.supportsIncrementalPublish = 'only'
 -- Hide all the normal export settings
 provider.showSections = { }
 
-function provider.startDialog(propertyTable)
+provider.exportPresetFields = {
+  { key = 'username', default = "Your username" },
+  { key = 'password', default = "Your password" },
+  { key = 'site_url', default = "http://www.myblog.com/" },
+  { key = 'wordpress_url', default = "" },
+}
+
+function provider.sectionsForBottomOfDialog(f, propertyTable)
+  return {
+    {
+      title = "Login Details",
+
+      synopsis = bind 'username',
+
+      f:column {
+        spacing = f:control_spacing(),
+
+        f:row {
+          spacing = f:label_spacing(),
+
+          f:static_text {
+            title = "Website:",
+            alignment = "right",
+            width = LrView.share "label_width",
+          },
+
+          f:edit_field {
+            width_in_chars = 40,
+            tooltip = "The address of the blog",
+            value = bind 'site_url',
+            validate = function(view, value)
+              if string.len(value) < 8 then
+                return false, value, "Website must be a valid website address"
+              end
+
+              if string.sub(value, 0, 7) ~= "http://" and string.sub(value, 0, 8) ~= "https://" then
+                return false, value, "Website must be a valid website address"
+              end
+
+              return true, value
+            end,
+          },
+        },
+
+        f:row {
+          spacing = f:label_spacing(),
+
+          f:static_text {
+            title = "Wordpress install:",
+            alignment = "right",
+            width = LrView.share "label_width",
+          },
+
+          f:edit_field {
+            width_in_chars = 40,
+            tooltip = "The address of the wordpress install, if different from the site's address",
+            value = bind 'wordpress_url',
+            validate = function(view, value)
+              if string.len(value) == 0 then
+                return true, value
+              end
+
+              if string.len(value) < 8 then
+                return false, value, "Wordpress address must be a valid website address"
+              end
+
+              if string.sub(value, 0, 7) ~= "http://" and string.sub(value, 0, 8) ~= "https://" then
+                return false, value, "Wordpress address must be a valid website address"
+              end
+
+              return true, value
+            end,
+          },
+        },
+
+        f:row {
+          spacing = f:label_spacing(),
+
+          f:static_text {
+            title = "Username:",
+            alignment = "right",
+            width = LrView.share "label_width",
+          },
+
+          f:edit_field {
+            width_in_chars = 20,
+            tooltip = "The username you use to log in to your blog",
+            value = bind 'username',
+            validate = function(view, value)
+              return string.len(value) > 0, value, "Username cannot be empty"
+            end,
+          },
+        },
+
+        f:row {
+          spacing = f:label_spacing(),
+
+          f:static_text {
+            title = "Password:",
+            alignment = "right",
+            width = LrView.share "label_width",
+          },
+
+          f:password_field {
+            width_in_chars = 20,
+            tooltip = "The password you use to log in to your blog",
+            value = bind 'password',
+            validate = function(view, value)
+              return string.len(value) > 0, value, "Password cannot be empty"
+            end,
+          },
+        },
+      },
+    },
+  }
 end
 
 function provider.endDialog(propertyTable)
-end
+  if string.sub(propertyTable.site_url, -1) ~= "/" then
+    propertyTable.site_url = propertyTable.site_url .. "/"
+  end
 
-function provider.sectionsForTopOfDialog(f, propertyTable)
-end
-
-function provider.sectionsForBottomOfDialog(f, propertyTable)
+  if string.len(propertyTable.wordpress_url) > 0 then
+    if string.sub(propertyTable.wordpress_url, -1) ~= "/" then
+      propertyTable.wordpress_url = propertyTable.wordpress_url .. "/"
+    end
+  end
 end
 
 provider.showSections = { }
@@ -35,7 +155,7 @@ provider.disableRenamePublishedCollection = false
 provider.disableRenamePublishedCollectionSet = false
 
 -- The setting to use for the publish service name if the user doesn't set one
-provider.publish_fallbackNameBinding = 'website'
+provider.publish_fallbackNameBinding = 'site_url'
 
 function provider.metadataThatTriggersRepublish(publishSettings)
   return {
